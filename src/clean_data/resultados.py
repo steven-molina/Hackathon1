a8834a0403f86879ef01d825c2abe47203dd06c7
danielDiloq <daniel@diloq.com>
2024-11-20 10:28:27 -0500

    --Requirements creado

--- src/clean_data/resultados.py
+++ src/clean_data/resultados.py
@@ -3,9 +3,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 class resultados:
     
-    def clean(self):
-        path= os.chdir(os.path.join(os.getcwd(),"../../"))
-        print(path)
+    def clean(self,data):
+        with open(os.path.join(BASE_DIR, 'SaberPro_Específicas_2016.txt'), 'r') as file:
+            data = file.read().replace('¬',',')
+        
+
 
 if __name__ == '__main__':
     resultados().clean()
